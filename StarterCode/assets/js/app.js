cd68a7bf3846a58ccdccce6a81b184df286821f5
aschultz10 <77344570+aschultz10@users.noreply.github.com>
2021-05-20 12:38:04 -0400

    calling data

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -15,11 +15,63 @@ var chartHeight = height - margin.top - margin.bottom;
 var chartWidth = width - margin.left - margin.right;
 
 // create svg container
-var svg = d3.select("body").append("svg")
+var svg = d3
+    .select("body")
+    .append("svg")
     .attr("height", height)
     .attr("width", width);
 
-// shift everything over by the margins
+// Append an SVG group
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("data.csv").then(function(err, ourdata) {
+    if (err) throw err;
+console.log(ourdata)
+
+    // parse data
+    ourdata.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
+  
+    // xLinearScale function above csv import
+    var xLinearScale = d3.scaleLinear().range([0, width]);
+    var yLinearScale = d3.scaleLinear().range([height, 0]);
+      
+  
+    // Create initial axis functions
+    var xaxis = d3.axisx(xLinearScale);
+    var yaxis = d3.axisy(yLinearScale);
+
+    var xaxismin;
+    var xaxismax;
+    var yaxismin;
+    var yaxismax;
+    
+    xaxismin = d3.min(ourdata, function(data) {
+return data.healthcare;
+    });
+    
+    xaxismax = d3.max(ourdata, function(data) {
+        return data.healthcare;
+    });
+    
+    yaxismin = d3.min(ourdata, function(data) {
+        return data.poverty;
+    });
+    
+    yaxismax = d3.max(ourdata, function(data) {
+        return data.poverty;
+    });
+    
+    xLinearScale.domain([xaxismin, xaxismax]);
+    yLinearScale.domain([yaxismin, yaxismax]);
+    console.log(xaxismin);
+    console.log(yaxismax);
+
 
